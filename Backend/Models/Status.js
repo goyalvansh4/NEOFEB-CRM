5929e167a183ce0c16d934bbf702616ff3a69340
Vansh Gupta <97226346+goyalvansh4@users.noreply.github.com>
2025-01-04 11:03:16 +0530

    Delete Backend directory

--- Backend/Models/Status.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("../Connection")
-
-const leadStatusSchema=new mongoose.Schema({
-    "leadStatus":{type:String,require:true,unique:true},
-    "description":{type:String},
-},{timestamps:true})
-
-const leadStatusModel =mongoose.model("LeadStatus",leadStatusSchema)
-module.exports=leadStatusModel;
